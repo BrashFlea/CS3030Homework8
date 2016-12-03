50dab0dc54dbdd8b142cb5cada1f43ac907ded6f
Bryson Oakley <brysonoakley@mail.weber.edu>
2016-12-03 22:37:33 +0000

    Finished run_report.sh

--- run_report.sh
+++ run_report.sh
@@ -36,8 +36,7 @@ do
 			;;
 		h) showHelp
 			;;
-		\?) echo "Unknown arg: $opt"
-			showHelp
+		\?) showHelp
 			;;
 	esac
 done
@@ -47,24 +46,46 @@ then
 	showHelp
 fi
 
-python3 create_report.py $begin $end
+fileDate=`date +%Y_%m_%d-%H:%m`
+file="report-$fileDate.txt"
+
+python3 create_report.py $begin $end > $file
 result=$?
+
+sed -i '1,3d' $file
 
 if [[ $result -eq 255 ]] # For some reaseon, if python exits with a negative number, it is just taken away from 256
 then
 	echo "Exited with -1. Sending an email..."
-	#mail -s "The create_report program exited with code -1" -r "run_report_script@yourscripts.net" $email << EOF
-	#The create_report could not run due to invalid date params. Please review it's documentation.
-#EOF
+	mail -s "The create_report program exited with code -1" -r "run_report_script@yourscripts.net" $email << EOF
+	The create_report could not run due to invalid date params. Please review it's documentation.
+EOF
 elif [[ $result -eq 254 ]]
 then
 	echo "Exited with -2. Sending an email..."
-	#mail -s "The create_report program exited with code -2" -r "run_report_script@yourscripts.net" $email << EOF
-	#The create_report program ran, but no transactions were found within the specified date range
-#EOF
+	mail -s "The create_report program exited with code -2" -r "run_report_script@yourscripts.net" $email << EOF
+	The create_report program ran, but no transactions were found within the specified date range
+EOF
 else
-	echo "All good"
+	echo "All good. Zipping and sending now"
+	zipFile="report-$fileDate.zip"
+	zip $zipFile $file
+	
+	ftpAddress=137.190.19.104
+	echo "Attempting to ftp..."
+	ftp -inv $ftpAddress << COMMS
+	user $user $password
+	put $zipFile
+	bye
+COMMS
+	rm $zipFile
+
+	mail -s "run_report.sh ran successfully to $ftpaddress" -r "run_report_script@yourscripts.net" $email << EOF
+	Successfully created a transaction report from $begin to $end
+EOF
 fi
+
+rm $file 
 
 exit 0
 
