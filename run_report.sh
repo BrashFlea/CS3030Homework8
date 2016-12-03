5fe7aabb03ee82869153e30656b1da46ebbc1512
Bryson Oakley <brysonoakley@mail.weber.edu>
2016-12-03 06:05:43 +0000

    Added in argument and help logic

--- run_report.sh
+++ run_report.sh
@@ -13,8 +13,46 @@
 
 #set -o nounset                              # Treat unset variables as an error
 
+showHelp() {
+	echo "This file will run the create_report.py file with the specified dates and ftp the results to the FTP server using the supplied credentials. Errors will be reported to the email"
+	echo "Usage: $0 -f <beginDate> -t <endDate> -e <email> -u <user> -p <password>"
+
+	exit -5
+}
 
 
+while getopts ":f:t:e:u:p:" opt
+do
+	case $opt in
+		f) begin=$OPTARG
+			;;
+		t) end=$OPTARG
+			;;
+		e) email=$OPTARG
+			;;
+		u) user=$OPTARG
+			;;
+		p) password=$OPTARG
+			;;
+		h) showHelp
+			;;
+		\?) echo "Unknown arg: $opt"
+			showHelp
+			;;
+	esac
+done
+
+if [[ $begin == "" || $end == "" || $email == "" || $user == "" || $password == "" ]]
+then
+	showHelp
+fi
+
+python3 create_report.py $begin $end
+
+if [[ $? -eq 0 ]]
+then
+	
+fi
 
 exit 0
 
