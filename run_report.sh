c05ba57dfb344962b31a5aae20506fee9804069e
bdnz <bd81217@icarus.cs.weber.edu>
2016-12-03 05:29:35 +0000

    Deniz Mod3

---
+++ run_report.sh
@@ -0,0 +1,20 @@
+#!/bin/bash - 
+#===============================================================================
+#
+#          FILE: run_report.sh
+# 
+#         USAGE: ./run_report.sh 
+# 
+#   DESCRIPTION: 
+# 
+#        AUTHOR: Bryson Oakley (), brysonoakley@mail.weber.edu
+#       CREATED: 12/03/2016 05:19
+#===============================================================================
+
+#set -o nounset                              # Treat unset variables as an error
+
+
+
+
+exit 0
+
